f9890fabf7e79c88847242912805c228deeb9332
Rafael Cruz <rafaeluzzi@gmail.com>
2018-05-05 15:34:01 -0400

    initial p

---
+++ index.php
@@ -0,0 +1,477 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="semantic/dist/semantic.min.js"></script>
+ <script type="text/javascript" src="//bitmovin-a.akamaihd.net/bitmovin-player/stable/7/bitmovinplayer.js"></script>
+    <style type="text/css">
+      #my-player{
+        width: 100%;
+      }
+      .bmpui-ui-watermark {
+  display: none;
+}
+.hide {
+
+display: none;
+}
+}
+.show {
+display: block;}
+}
+.bmpui-ui-skin-modern .bmpui-ui-seekbar-label .bmpui-seekbar-label-inner>.bmpui-container-wrapper .bmpui-seekbar-label-metadata{
+  background:#ffffff !important;
+  color: #333 !important;
+}
+.bmpui-ui-skin-modern .bmpui-ui-controlbar.bmpui-hidden, .vshow {
+opacity: 1 !important;
+visibility: visible !important;}
+    </style>
+</head>
+<body>
+<div class="ui">
+<div class="ui grid container">
+     <div class="sixteen wide column">
+     <h3 class="ui header" style="margin-top: 40px">Testing player interaction functions</h3>
+
+       <!-- <div class="ui five statistics">
+      <?php $all=5;for($i=0;$i < $all;$i++){
+            echo "<div class=\"statistic\">
+            <a>
+              <div class=\"value\">
+                <img src=\"/images/avatar/small/joe.jpg\" class=\"ui circular inline image\">
+                ".($i + 1)."
+              </div>
+              <div class=\"label\">
+                Team Members
+              </div>
+            </a>
+            </div>";
+      };?>
+
+</div> -->
+<div class="ui message">
+  <div class="header">
+    Output
+  </div>
+  <div id="event">
+  Timestamp
+</div>
+<div id="view">
+Viewing Angle
+</div>
+<div id="time">
+00:00
+</div>
+</div>
+<h3 class="ui dividing header">Testing viewing direction change from outside player </h3>
+     <a class="ui image label" onclick="getAngle(30);">
+  <img src="http://semantic-ui.com/images/avatar/small/steve.jpg">
+  Quique Sombrero
+  <div class="detail">Student</div>
+</a>
+<a class="ui image label" onclick="getAngle(300);">
+  <img src="http://semantic-ui.com/images/avatar/small/joe.jpg">
+  Ramon Rapero
+  <div class="detail">Student</div>
+</a>
+<a class="ui image label" onclick="getAngle(225);">
+  <img src="http://semantic-ui.com/images/avatar/small/elliot.jpg">
+  Luis Gaban
+  <div class="detail">Student</div>
+</a>
+<a class="ui image label" onclick="getAngle(150);">
+  <img src="http://semantic-ui.com/images/avatar/small/matt.jpg">
+  Pedro Tirantes
+  <div class="detail">Student</div>
+</a>
+<a class="ui image label" onclick="getAngle(90);">
+  <img src="http://semantic-ui.com/images/avatar/small/christian.jpg">
+  Juan Whisky
+  <div class="detail">Student</div>
+</a>
+     </div>
+</div>
+<div class="ui grid container">
+<!-- flags -->
+
+<!-- /flags -->
+<div class="nine wide column">
+   <h3 class="ui dividing header">360/VR Player </h3>
+    <div id="my-player"></div>
+
+</div>
+<div class="three wide column">
+  <h3 class="ui dividing header">Now viewing</h3>
+  <div id="guy"></div>
+</div>
+<div class="four wide column">
+  <div class="ui comments">
+  <h3 class="ui dividing header">Jump to tag from outside player </h3>
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/matt.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Matt</a>
+      <div class="metadata">
+        <span class="date">Today at 12:05PM</span>
+      </div>
+      <div class="text">
+        Big tantrum
+      </div>
+            <div class="actions">
+            <a onclick="sk(15);">
+               <i class="file video outline icon"></i>
+                View
+              </a>
+              <a>
+                <i class="time icon"></i>
+                 00:15
+              </a>
+            </div>
+    </div>
+  </div>
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/elliot.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Elliot Fu</a>
+      <div class="metadata">
+        <span class="date">Today at 12:30PM</span>
+      </div>
+      <div class="text">
+        <p>Task avoidance, won't coporate.</p>
+      </div>
+          <div class="actions">
+            <a onclick="sk(25);">
+               <i class="file video outline icon"></i>
+                View
+              </a>
+              <a>
+                <i class="time icon"></i>
+                 00:25
+              </a>
+            </div>
+    </div>
+
+      <div class="comment">
+        <a class="avatar">
+          <img src="http://semantic-ui.com/images/avatar/small/jenny.jpg">
+        </a>
+        <div class="content">
+          <a class="author">Jenny Hess</a>
+          <div class="metadata">
+            <span class="date">Just now</span>
+          </div>
+          <div class="text">
+            Very loud verbal aggression
+          </div>
+            <div class="actions">
+            <a onclick="sk(30);">
+               <i class="file video outline icon"></i>
+                View
+              </a>
+              <a>
+                <i class="time icon"></i>
+                 00:30
+              </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/joe.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Joe Henderson</a>
+      <div class="metadata">
+        <span class="date">5 days ago</span>
+      </div>
+      <div class="text">
+        Self injury on his hand
+      </div>
+          <div class="actions">
+            <a onclick="sk(38);">
+               <i class="file video outline icon"></i>
+                View
+              </a>
+              <a>
+                <i class="time icon"></i>
+                 00:38
+              </a>
+            </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+
+<div class="ui grid container">
+ <div class="sixteen wide column">
+ <h3 class="ui dividing header">Timestamps display 3s pre and 3s after</h3>
+   <div id="careflag"></div>
+   <div id="flag1" class="ui comments hide">
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/stevie.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Stevie Feliciano</a>
+      <div class="metadata">
+        <div class="date">2 days ago</div>
+        <div class="rating">
+          <i class="star icon"></i>
+          5 Faves
+        </div>
+      </div>
+      <div class="text">
+        Hey guys, I hope this example comment is helping you read this documentation.
+      </div>
+    </div>
+  </div>
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/matt.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Matt</a>
+      <div class="metadata">
+        <span class="date">Today at 5:42PM</span>
+      </div>
+      <div class="text">
+        How artistic!
+      </div>
+      <div class="actions">
+        <a class="reply">Reply</a>
+      </div>
+    </div>
+  </div>
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/elliot.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Elliot Fu</a>
+      <div class="metadata">
+        <span class="date">Yesterday at 12:30AM</span>
+      </div>
+      <div class="text">
+        <p>This has been very useful for my research. Thanks as well!</p>
+      </div>
+      <div class="actions">
+        <a class="reply">Reply</a>
+      </div>
+    </div>
+    <div class="comments">
+      <div class="comment">
+        <a class="avatar">
+          <img src="http://semantic-ui.com/images/avatar/small/jenny.jpg">
+        </a>
+        <div class="content">
+          <a class="author">Jenny Hess</a>
+          <div class="metadata">
+            <span class="date">Just now</span>
+          </div>
+          <div class="text">
+            Elliot you are always so right :)
+          </div>
+          <div class="actions">
+            <a class="reply">Reply</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/joe.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Joe Henderson</a>
+      <div class="metadata">
+        <span class="date">5 days ago</span>
+      </div>
+      <div class="text">
+        Dude, this is awesome. Thanks so much
+      </div>
+      <div class="actions">
+        <a class="reply">Reply</a>
+      </div>
+    </div>
+</div>
+ </div>
+ <!-- flag 2 -->
+ <div id="flag2" class="ui comments hide">
+  <div class="comment">
+    <a class="avatar">
+      <img src="http://semantic-ui.com/images/avatar/small/stevie.jpg">
+    </a>
+    <div class="content">
+      <a class="author">Stevie Feliciano</a>
+      <div class="metadata">
+        <div class="date">3 days ago</div>
+        <div class="rating">
+          <i class="star icon"></i>
+          8 Faves
+        </div>
+      </div>
+      <div class="text">
+        Hey this was unexpected!!!
+      </div>
+    </div>
+  </div>
+  </div>
+     <div class="sixteen wide column">
+  <h3 class="ui dividing header">Testing getting the current video time for adding timestamps from outside the player </h3>
+
+<button class="ui button" onclick="getTime();">Get Current Timestamp</button>
+
+<button class="ui button" onclick="getViewAngle();">Get Viewing Angle</button>
+</div>
+</div>
+
+</div><!-- /container -->
+<!-- Here will be the player setup script -->
+<script type="text/javascript">
+  var conf = {
+  key: '866d0882-a04a-4474-886e-80cdff0a4221',
+  events: {
+        onReady: function () {
+                player.play();
+        }
+    },
+    style: {
+    ux                      : true,
+    autoHideControls: false,
+  },
+  source: {
+        title: "Teletherapy Player " + bitmovin.player.version,
+        description: "VR/360° Example",
+        /*dash: '//bitmovin-a.akamaihd.net/content/vr-player/playhouse-vr/mpds/105560.mpd',
+        hls: '//bitmovin-a.akamaihd.net/content/vr-player/playhouse-vr/m3u8s/105560.m3u8',*/
+        progressive: '/classroom.mp4',
+        //poster: '//bitmovin-a.akamaihd.net/content/vr-player/playhouse-vr/poster.jpg',
+        vr: {
+            startPosition: 0,
+            contentType: bitmovin.player.VR.CONTENT_TYPE.SINGLE,
+            initialRotation: true,
+            initialRotateRate: 0.025,
+        },
+
+        markers: [
+      {time:15,   title: 'Tantrum'},
+      {time:25, title: 'Task Avoidance'},
+      {time:30, title: 'Verbal Aggression'},
+      {time:38, title: 'Self Injury'}
+    ]
+      }
+};
+var persons = {"a":{"name":"Quique Sombrero","age":"35","desc":"I started painting as a hobby when I was little. I didn't know I had any talent."},
+"b":{"name":"Juan Whisky","age":"29","desc":"You could sit here for weeks with your one hair brush trying to do that - or you could do it with one stroke with an almighty brush."},"c":{"name":"Pedro Tirantes","age":"23","desc":"Now let's put some happy little clouds in here. It is a lot of fun."},"d":{"name":"Luis Gaban","age":"31","desc":"We can fix anything. Even trees need a friend. We all need friends."},"e":{"name":"Ramon Rapero","age":"27","desc":"Work on one thing at a time. Don't get carried away - we have plenty of time. Little trees and bushes grow however makes them happy."}};
+var player = bitmovin.player('my-player');
+function sk(timestamp){
+        player.seek(timestamp);
+     }
+player.addEventHandler(bitmovin.player.EVENT.ON_SEEKED, function(seekEvent){
+    updateTime(JSON.stringify(player.getCurrentTime()));
+});
+player.addEventHandler(bitmovin.player.EVENT.ON_VR_VIEWING_DIRECTION_CHANGE, function(viewEvent){
+    updateViewAngle(viewEvent.direction);
+});
+player.addEventHandler(bitmovin.player.EVENT.ON_TIME_CHANGED, function(time){
+
+    updateTimer(time);
+});
+//bitmovin.player.EVENT.ON_TIME_CHANGED
+
+//Helper method:
+function updateTime(time) {
+
+    document.getElementById("event").innerHTML ="The video has been seeked to "+JSON.stringify(player.getCurrentTime())+"s";
+
+}
+function updateViewAngle(view) {
+    updatePerson(view);
+    document.getElementById("view").innerHTML ="View angle changed to "+view.yaw;
+}
+function updateTimer(time) {
+    if(time.time > 12.0000 && time.time < 18.0000){
+     document.getElementById("careflag").innerHTML =JSON.stringify(conf.source.markers[0]);
+     $('#flag1').removeClass('hide').addClass('show');
+;
+   }else{
+     document.getElementById("careflag").innerHTML ="";
+     $('#flag1').removeClass('show').addClass('hide');
+   }
+
+   if(time.time > 22.0000 && time.time < 28.0000){
+     document.getElementById("careflag").innerHTML =JSON.stringify(conf.source.markers[1]);
+     $('#flag2').removeClass('hide').addClass('show');
+;
+   }else{
+     document.getElementById("careflag").innerHTML ="";
+     $('#flag2').removeClass('show').addClass('hide');
+   }
+
+    document.getElementById("time").innerHTML ="Now playing time: "+time.time;
+}
+
+function getTime(){
+   alert(JSON.stringify(player.getCurrentTime()));
+}
+function getViewAngle(){
+  alert(JSON.stringify(player.vr.getViewingDirection()));
+}
+function getAngle(angle){
+  player.vr.setViewingDirection({pitch:0,roll:0,yaw:angle});
+  updateViewAngle({pitch:0,roll:0,yaw:angle});
+}
+function updatePerson(view){
+  if((view.yaw >= 0 && view.yaw <= 55) || (view.yaw >= 351 && view.yaw <= 360) || (view.yaw >= -360 && view.yaw <= -305) || (view.yaw >= -11 && view.yaw <= -0)){
+    var template = "<div class=\"ui card\"><div class=\"image\"><img src=\"http://semantic-ui.com/images/avatar/large/steve.jpg\"></div><div class=\"content\"><div class=\"center aligned header\">"+persons.a.name+"</div><div class=\"center aligned description\"><p>"+persons.a.desc+"</p></div></div></div>";
+    document.getElementById("guy").innerHTML = template;
+
+   }else if((view.yaw >= 56 && view.yaw <= 115) || (view.yaw >= -304 && view.yaw <= -245)){
+    var template = "<div class=\"ui card\"><div class=\"image\"><img src=\"http://semantic-ui.com/images/avatar/large/christian.jpg\"></div><div class=\"content\"><div class=\"center aligned header\">"+persons.b.name+"</div><div class=\"center aligned description\"><p>"+persons.b.desc+"</p></div></div></div>";
+    document.getElementById("guy").innerHTML = template;
+
+   }else if((view.yaw >= 116 && view.yaw <= 175) || (view.yaw >= -244 && view.yaw <= -185)){
+    var template = "<div class=\"ui card\"><div class=\"image\"><img src=\"http://semantic-ui.com/images/avatar/large/matt.jpg\"></div><div class=\"content\"><div class=\"center aligned header\">"+persons.c.name+"</div><div class=\"center aligned description\"><p>"+persons.c.desc+"</p></div></div></div>";
+    document.getElementById("guy").innerHTML = template;
+
+   }else if((view.yaw >= 176 && view.yaw <= 250) || (view.yaw >= -184 && view.yaw <= -110)){
+    var template = "<div class=\"ui card\"><div class=\"image\"><img src=\"http://semantic-ui.com/images/avatar/large/elliot.jpg\"></div><div class=\"content\"><div class=\"center aligned header\">"+persons.d.name+"</div><div class=\"center aligned description\"><p>"+persons.d.desc+"</p></div></div></div>";
+    document.getElementById("guy").innerHTML = template;
+
+   }else if((view.yaw >= 251 && view.yaw <= 350) || (view.yaw >= -109 && view.yaw <= -10)){
+    var template = "<div class=\"ui card\"><div class=\"image\"><img src=\"http://semantic-ui.com/images/avatar/large/joe.jpg\"></div><div class=\"content\"><div class=\"center aligned header\">"+persons.e.name+"</div><div class=\"center aligned description\"><p>"+persons.e.desc+"</p></div></div></div>";
+    document.getElementById("guy").innerHTML = template;
+
+   }else{
+    document.getElementById("guy").innerHTML = "";
+   };
+}
+player.setup(conf).then(
+  function(playerInstance) {
+    console.log('Successfully created Bitmovin Player instance');
+
+
+  },
+  function(reason) {
+    console.log('Error while creating Bitmovin Player instance');
+  }
+);
+</script>
+
+</body>
+</html>
